37ff0609c328ad29547ed7498c79c5d9c228117d
Robert de Forest <forestr@amazon.com>
2017-04-02 10:39:44 -0700

    forgot to save and commit corvid schema

--- janus/corvid-schema.groovy
+++ janus/corvid-schema.groovy
@@ -16,23 +16,31 @@
 
 def defineGratefulDeadSchema(janusGraph) {
     m = janusGraph.openManagement()
+
     // vertex labels
-    euclidic = m.makeVertexLabel("euclidic").make()
-    relation = m.makeVertexLabel("song").make()
+    referant  = m.makeVertexLabel("referant").make()
+    relation  = m.makeVertexLabel("relation").make()
+    event     = m.makeVertexLabel("event").make()
+
     // edge labels
-    sungBy     = m.makeEdgeLabel("sungBy").make()
-    writtenBy  = m.makeEdgeLabel("writtenBy").make()
-    followedBy = m.makeEdgeLabel("followedBy").make()
+    reference = m.makeEdgeLabel("reference").make()
+    transform = m.makeEdgeLabel("transform").make()
+    change    = m.makeEdgeLabel("change").make()
+
     // vertex and edge properties
-    blid         = m.makePropertyKey("bulkLoader.vertex.id").dataType(Long.class).make()
-    name         = m.makePropertyKey("name").dataType(String.class).make()
-    songType     = m.makePropertyKey("songType").dataType(String.class).make()
-    performances = m.makePropertyKey("performances").dataType(Integer.class).make()
-    weight       = m.makePropertyKey("weight").dataType(Integer.class).make()
+    //   referant
+    name      = m.makePropertyKey("name").dataType(String.class).make()
+    desc      = m.makePropertyKey("desc").dataType(String.class).make()
+
+    //   relation
+    relation  = m.makePropertyKey("relation").dataType(String.class).make()
+
+    //   change
+    instant   = m.makePropertyKey("instant").dataType(String.class).make()
+
     // global indices
-    m.buildIndex("byBulkLoaderVertexId", Vertex.class).addKey(blid).buildCompositeIndex()
-    m.buildIndex("artistsByName", Vertex.class).addKey(name).indexOnly(artist).buildCompositeIndex()
-    m.buildIndex("songsByName", Vertex.class).addKey(name).indexOnly(song).buildCompositeIndex()
+    m.buildIndex("referantsByName", Vertex.class).addKey(name).indexOnly(referant).buildCompositeIndex()
+
     // vertex centric indices
     m.buildEdgeIndex(followedBy, "followedByWeight", Direction.BOTH, Order.decr, weight)
     m.commit()
